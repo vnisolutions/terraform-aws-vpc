4a790c373c1d9c5771d3947a5100a48ff2a38d1b
lamvini <lamvini.hcmus@gmail.com>
2023-01-02 21:49:50 +0700

    update module format

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,19 @@
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
+
+output "database_subnets" {
+  value = module.vpc.database_subnets
+}
+
+output "elasticache_subnets" {
+  value = module.vpc.elasticache_subnets
+}
